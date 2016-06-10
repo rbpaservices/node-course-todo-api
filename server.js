4a723895d8ca6d6bd0ff166e5628817d2e8af30a
Robert Brown <rbpaservices@gmail.com>
2016-06-10 10:07:03 -0400

    Add _.where Search

--- server.js
+++ server.js
@@ -19,7 +19,34 @@ app.get('/', function(req, res){
 });
 
 app.get('/todos', function(req, res){
-	res.json(todos);
+
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') ){
+
+			if (queryParams.completed === 'true'){
+
+				filteredTodos =_.where(filteredTodos, {completed: true});
+			} else {
+				if (queryParams.completed === 'false') {
+
+					filteredTodos =_.where(filteredTodos, {completed: false});
+
+				}
+			}
+	}
+
+	if (queryParams.hasOwnProperty('description') ){
+
+			if (_.isString(queryParams.description) && queryParams.description.trim().length > 0){
+				
+				filteredTodos =_.where(filteredTodos, {description: queryParams.description});
+			} 
+	}
+
+
+	res.json(filteredTodos);
 });
 
 app.get('/todos/:id', function(req, res){
